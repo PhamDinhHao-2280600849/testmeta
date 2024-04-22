d16732c2e44b28f6930842511129b1d149858e67
PhamDinhHao_2280600849 <quangkhai336682@gmail.com>
2024-04-22 10:47:53 +0700

    hao1

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,6 +1,6 @@
 
 import type { Metadata, ResolvingMetadata } from 'next'
-import useSWR,{Fetcher} from "swr";
+// import useSWR,{Fetcher} from "swr";
 export const openGraphImage = { images: ['https://www.vjp-connect.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogobanner.dcf00dbd.png&w=3840&q=75'] }
 type Props = {
   params: { id: string }
@@ -19,38 +19,38 @@ export async function generateMetadata(
  
   // optionally access and extend (rather than replace) parent metadata
   const previousImages = (await parent).openGraph?.images || []
- 
+   
   return {
     title: product.title,
     openGraph: {
         ...openGraphImage
     },
-    
   }
+  
 }
-const ViewDetail = ({ params }: { params: { id: string } }) => {
-    const fetcher:Fetcher<IBlog,string> = (url: string) => fetch(url).
-        then((res) => res.json());
+// const ViewDetail = ({ params }: { params: { id: string } }) => {
+//     const fetcher:Fetcher<IBlog,string> = (url: string) => fetch(url).
+//         then((res) => res.json());
 
 
-    const { data, error, isLoading } = useSWR(`http://localhost:8000/blogs/${params.id}`, fetcher,
-        {
-            revalidateIfStale: false,
-            revalidateOnFocus: false,
-            revalidateOnReconnect: false
-        });
-    if (isLoading) {
-        return <div>loading...</div>
-    }
+//     const { data, error, isLoading } = useSWR(`http://localhost:8000/blogs/${params.id}`, fetcher,
+//         {
+//             revalidateIfStale: false,
+//             revalidateOnFocus: false,
+//             revalidateOnReconnect: false
+//         });
+//     if (isLoading) {
+//         return <div>loading...</div>
+//     }
 
-    return (
-        <div>
-            {data?.title}
-            {data?.content}
-            {data?.author}
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             {data?.title}
+//             {data?.content}
+//             {data?.author}
+//         </div>
+//     )
+
 
  
 export default function Page({ params, searchParams }: Props) {}
